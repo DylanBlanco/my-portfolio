fae46d4f9dd30517d7141523d68e8213c5a4f96d
DylanBlanco <dylanblanco1198@gmail.com>
2025-07-02 16:44:29 +0200

    Menu Button

--- src/Main_Components/NavBar.jsx
+++ src/Main_Components/NavBar.jsx
@@ -53,14 +53,13 @@ const Navbar = () => {
                 </button>
 
                 {/* Mobile Menu Button */}
-                {/* <div className="md:hidden"> */}
-                {/* <div className="md">
+                <div className="md:hidden">
                         <button className="text-gray-700 hover:text-orange-500 focus:outline-none">
                             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
                         </button>
-                </div> */}
+                </div>
             </div>
         </nav>
     );
